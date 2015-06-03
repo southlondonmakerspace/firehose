209a4aff0041453f159ca60d0778bba1833e8645
Tomnewsom <tom.newsom@gmail.com>
2015-06-03 15:25:20 +0100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,1 +1,7 @@
+# name: firehose
+# about: plugin to view all recent content regardless of source
+# version: 0.01
+# authors: tomnewsom
 
+
+register_asset "javascripts/discourse/templates/connectors/admin-menu/admin-menu.hbs"
